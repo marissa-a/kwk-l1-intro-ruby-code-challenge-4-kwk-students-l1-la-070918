018a91a0714a6bfb918e1d5941975f063f3da952
marissa-a <marissa.andrade@verizon.net>
2018-07-11 23:49:42 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,23 @@
 #write out your code here
+def least_coins
+  coins={:quarters =>0, :dimes =>0, :nickles =>0, :pennies =>0}
+  cents=29
+  while cents>=25
+  coins[:quarters[0]]=1
 
-def least_coins(cents)
-
-#Code your answer here!
-
+  cents-25
 end
+  while cents>=10
+  coins[:dimes[1]]=1
+  cents-10
+end
+  while cents>=5
+  coins[:nickles[2]]=1
+  cents-5
+end
+  while cents>=1
+  coins[:pennies[3]]=1
+  cents-1
+end
+end
+puts least_coins
